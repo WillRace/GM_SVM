2784d76e971d1fca6cb5265610f17288c2f3129f
William Race <70697507+WillRace@users.noreply.github.com>
2021-10-04 21:48:50 +0100

    Add files via upload

---
+++ Basic_SVM.R
@@ -0,0 +1,136 @@
+package.loader <- function(pack, library = F){
+  checkInstall <- pack[!(pack %in% installed.packages()[, "Package"])]
+  if(length(checkInstall))
+    install.packages(checkInstall, dependencies = T)
+  if(library == T)
+    sapply(pack, require, character.only = T)
+  }
+
+package.loader("mlr", library = T)
+package.loader(c("caret"))
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                                     #
+# A function for performing cross validated SVM modeling on GM data, with PCA feature extraction as a #
+# pre-processing step. Can be applied to a dataset for 'Leave One Out' CV through use of the 'lapply' #
+# function given a vector of integers corresponding to test individuals eg:                           #
+#                                                                                                     #
+# testers <- c(1:90); lapply(testers, gm.pca.SVM, data = svmData, pcSelect = c(1:10), plotSVM = T)    #
+#                                                                                                     #
+# GM data should be provided in the form of a dataframe where the first variable is a class variable  #
+# and subsequent variables are numeric (centroid size and procrustes shape coordinates).              #
+#                                                                                                     #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+gm.SVM <- function(i, data){
+  
+  set.seed(123)
+  
+  print(paste("Sampling ID:", i, "from data"))
+  
+  ### Pre-processing ###
+  
+  # Train/Test split #
+  
+  train <- data[-i,] # Removes test items from training set
+  test <- data[i,-1] # Creates a new dataset containing test items without labels
+  
+  # Normalise Data #
+  
+  for(n in names(train[,-1])){
+    test[,n] <- (test[,n] - min(train[,n])) / (max(train[,n]) - min(train[,n]))
+    train[,n] <- (train[,n] - min(train[,n])) / (max(train[,n]) - min(train[,n]))
+  }
+  
+  ### SVM Section ###
+  
+  # Hyperparameter Tuning #
+  
+  svmTask <- makeClassifTask(data = train, 
+                             target = "target")
+  
+  svmLearner <- makeLearner("classif.svm", predict.type = "response")
+  
+  svmResample <- makeResampleDesc("RepCV", reps = 5, folds = 5, stratify = T)
+
+  svmControl <- makeTuneControlGrid(resolution = 10L)
+  
+  svmParams <- makeParamSet(
+    makeDiscreteParam("kernel", values = "linear"),
+    makeNumericParam("cost", lower = -5, upper = 5, trafo = function(x) 2^x))
+  
+  tunedSVMParams <- tuneParams("classif.svm", task = svmTask, 
+                               resampling = svmResample,
+                               par.set = svmParams,
+                               control = svmControl,
+                               measures = list(f1))
+  
+  tunedSVM <- setHyperPars(svmLearner, par.vals = tunedSVMParams$x)
+  
+  # Training and testing #
+  
+  modelSVM <- train(tunedSVM, svmTask)
+  
+  testSVM <- predict(object = modelSVM, newdata = test)
+  
+  ### Function Returns ###
+  
+  return(list("Observed Genus" = data[i,1],
+              "SVM Result" = as.character(testSVM[["data"]][['response']]),
+              "test.train" = list("train" = train,
+                                  "test" = test)))
+}
+
+
+# LOO Cross Validation -----------------------------------------------------------------------------
+
+cross.validate <- function(data){
+  
+  parallelMap::parallelStartSocket(cpus = parallel::detectCores())
+  
+  testers <- c(1:nrow(data))
+  
+  results <- lapply(testers, 
+                    gm.SVM, 
+                    data = data)
+  
+  parallelMap::parallelStop()
+  
+  results <- data.frame("Actual Class" = sapply(results, '[[', 1),
+                        "Predicted Class" = sapply(results, '[[', 2))
+  
+  caretMatrix <- caret::confusionMatrix(table(resultsLM1[,1], 
+                                              resultsLM1[,2]))
+  
+  return(list("Result" = results,
+              "Caret Stats" = caretMatrix))
+}
+
+
+# PCA Score Test -----------------------------------------------------------------------------------
+
+pc.score.test <- function(data){
+  
+  testScores <- seq(2, round(nrow(data)*0.90, 0), by = 1)
+  
+  scoreResults <- data.frame(Scores = rep(NA, length(testScores)), 
+                             Acc_Ovr = rep(NA, length(testScores)))
+  
+  testers <- c(1:nrow(data))
+  
+  parallelMap::parallelStartSocket(cpus = parallel::detectCores())
+  
+  for(i in c(1:length(testScores))){
+    results <- lapply(testers, gm.LDA, data, prePCA = T, pcSelect = c(1:testScores[i]))
+    resultsLM1 <- data.frame("True Class" = sapply(results, '[[', 1),
+                             "Predicted Class" = sapply(results, '[[', 2))
+    shapeMatrix <- caret::confusionMatrix(table(resultsLM1[,1], resultsLM1[,2]))
+    scoreResults[i,1] <- testScores[i]
+    scoreResults[i,2] <- as.numeric(shapeMatrix$overall[1])
+  }
+  
+  parallelMap::parallelStop()
+  
+  return(list("results" = scoreResults))
+  
+}
